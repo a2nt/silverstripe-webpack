e8ec8a0b1c1f504c4b384e930a264003e65792e2
Tony Air <tony@thewebdevelopmentagency.com>
2018-05-13 22:32:50 +0700

    Minor update

--- site/_config.php
+++ site/_config.php
@@ -1,1 +1,15 @@
 <?php
+
+use SilverStripe\Forms\HTMLEditor\HtmlEditorConfig;
+use SilverStripe\Core\Manifest\ModuleResourceLoader;
+
+HtmlEditorConfig::get('cms')->enablePlugins([
+    'template',
+    'fullscreen',
+    'hr',
+    'contextmenu',
+    'charmap',
+    'visualblocks',
+    'lists',
+    'charcount' => ModuleResourceLoader::resourceURL('drmartingonzo/ss-tinymce-charcount:client/dist/js/bundle.js'),
+]);
