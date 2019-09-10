bfa082bca3998dd9f8c1cc26ad9aaeac9ac848a6
Tony Air <tony@thewebdevelopmentagency.com>
2019-09-10 17:53:31 +0700

    Fix: SiteTreeExtension

--- app/src/Extensions/SiteTreeExtension.php
+++ app/src/Extensions/SiteTreeExtension.php
@@ -2,13 +2,9 @@
 
 namespace Site\Extensions;
 
+use SilverStripe\Forms\TextareaField;
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TreeMultiselectField;
-use Sheadawson\Linkable\Models\Link;
-use SilverStripe\Forms\TextField;
-use Sheadawson\Linkable\Forms\LinkField;
 
 class SiteTreeExtension extends DataExtension
 {
