97d4ffca26dd5e6e708c51335535f2bc9c76839c
Tony Air <tony@twma.pro>
2021-09-05 21:46:32 +0200

    UI updates

--- app/_config.php
+++ app/_config.php
@@ -1,1 +1,7 @@
 <?php
+
+/*\SilverStripe\Core\Config\Config::modify()->set(
+    \SilverStripe\Control\Email\Email::class,
+    'bcc_all_emails_to',//'cc_all_emails_to',
+    'test@example.com'
+);*/
