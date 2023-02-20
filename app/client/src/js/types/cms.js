36d72210f741491f5ba3e917e798d581897cbe15
Tony Air <tony@twma.pro>
2023-02-20 16:55:17 +0200

    IMPR: CMS lightbox

--- app/client/src/js/types/cms.js
+++ app/client/src/js/types/cms.js
@@ -1,4 +1,16 @@
 'use strict'
 
 import '../../scss/cms.scss'
+import "@a2nt/meta-lightbox-js/src/js/app";
 // import '@a2nt/ss-bootstrap-ui-webpack-boilerplate/src/js/_components/_ui.image-position';
+
+(function ($) {
+    $.entwine("ss", ($) => {
+        $('[data-toggle="lightbox"]').entwine({
+            onmatch() {
+                console.log("Init lightbox links at CMS");
+                window.dispatchEvent(new Event("MetaWindow.initLinks"));
+            },
+        });
+    });
+})(jQuery);
