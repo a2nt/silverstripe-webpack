3e5c6af1fbd2d5a13357d782d2f8a497db8f6b33
Tony Air <tony@twma.pro>
2023-10-25 00:23:16 +0200

    IMPR: GraphQL Live tests fixtures

--- app/client/src/js/_graphql.js
+++ app/client/src/js/_graphql.js
@@ -1,8 +1,11 @@
 import '@a2nt/mithril-ui/src/js/ui'
 const m = require("mithril") // eslint-disable-line
 
-const MainContent = document.getElementById('MainContent')
+const MainContentContainer = document.getElementById('MainContent')
+
+const MainContent = document.createElement('div')
 MainContent.classList.add('page--container')
+MainContentContainer.append(MainContent)
 
 const page = require('@a2nt/mithril-ui/src/js/ui/page/tpl')
 m.mount(MainContent, page)
