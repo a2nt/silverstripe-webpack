051a126624c0398bafec432cf4e1e73c5bb084e7
Tony Air <tony@twma.pro>
2023-10-26 05:26:24 +0200

    IMPR: GraphQL and modules

--- app/src/Pages/Page.php
+++ app/src/Pages/Page.php
@@ -4,12 +4,8 @@
 // extends global Page class
 //namespace App\Pages;
 
-use DNADesign\Elemental\Models\ElementContent;
 use SilverStripe\CMS\Model\SiteTree;
-use SilverStripe\Control\Controller;
 use SilverStripe\FontAwesome\FontAwesomeField;
-use SilverStripe\GraphQL\Controller as GraphQLController;
-use TractorCow\Fluent\Extension\FluentSiteTreeExtension;
 
 /**
  * Class \Page
@@ -22,12 +18,14 @@ use TractorCow\Fluent\Extension\FluentSiteTreeExtension;
  * @method \DNADesign\Elemental\Models\ElementalArea ElementalArea()
  * @mixin \DNADesign\Elemental\Extensions\ElementalPageExtension
  * @mixin \A2nt\CMSNiceties\Widgets\WidgetPageExtension
+ * @mixin \A2nt\CMSNiceties\GraphQL\ObjectGraphQlEx
  * @mixin \Sheadawson\Linkable\Extensions\LinkableDataExtension
  */
 class Page extends SiteTree
 {
-    protected $_cached = [];
-    private static $default_container_class = 'container';
+    private static $mailchimp_list_id = '0c122fd88f';
+    private static $mailchimp_key = 'e2c03357299701a82275d6de9c8f2551-us8';
+
     private static $db = [
         'BlockIcon' => 'Varchar(255)',
     ];
@@ -35,11 +33,6 @@ class Page extends SiteTree
     private static $field_include = [
         'ElementalAreaID',
     ];
-
-    public static function DefaultContainer()
-    {
-        return self::config()->get('default_container_class');
-    }
 
     public function getSettingsFields()
     {
@@ -50,62 +43,5 @@ class Page extends SiteTree
         ]);
 
         return $fields;
-    }
-
-    /*
-     * Shows custom summary of the post, otherwise
-     * Displays summary of the first content element
-     */
-    public function Summary($wordsToDisplay = 30)
-    {
-        if (isset($this->_cached['summary' . $wordsToDisplay])) {
-            return $this->_cached['summary' . $wordsToDisplay];
-        }
-
-        $summary = $this->getField('Summary');
-        if ($summary) {
-            $this->_cached['summary' . $wordsToDisplay] = $summary;
-
-            return $this->_cached['summary' . $wordsToDisplay];
-        }
-
-        $element = ElementContent::get()->filter([
-            'ParentID' => $this->ElementalArea()->ID,
-            'HTML:not' => [null],
-        ])->first();
-
-        if ($element) {
-            $this->_cached['summary' . $wordsToDisplay] = $element->dbObject('HTML')->Summary($wordsToDisplay);
-
-            return $this->_cached['summary' . $wordsToDisplay];
-        }
-
-        $content = $this->getField('Content');
-        if ($content) {
-            $this->_cached['summary' . $wordsToDisplay] = $this->dbObject('Content')->Summary($wordsToDisplay);
-
-            return $this->_cached['summary' . $wordsToDisplay];
-        }
-
-        $this->_cached['summary' . $wordsToDisplay] = false;
-
-        return $this->_cached['summary' . $wordsToDisplay];
-    }
-
-    public function CSSClass()
-    {
-        return str_replace(['\\'], '-', $this->getField('ClassName'));
-    }
-
-    protected function onBeforeWrite()
-    {
-        parent::onBeforeWrite();
-
-        if (class_exists(FluentSiteTreeExtension::class) && ! $this->isDraftedInLocale() && $this->isInDB()) {
-            $elementalArea = $this->ElementalArea();
-
-            $elementalAreaNew = $elementalArea->duplicate();
-            $this->setField('ElementalAreaID', $elementalAreaNew->ID);
-        }
     }
 }
