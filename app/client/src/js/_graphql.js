e0410c56671549ad0c4ce7d8d629248417b1bdb2
Tony Air <tony@twma.pro>
2023-11-27 17:22:06 +0200

    IMPR: Frontend update

--- app/client/src/js/_graphql.js
+++ app/client/src/js/_graphql.js
@@ -6,21 +6,21 @@ const MainContentContainer = document.getElementById('MainContent')
 
 const MainContent = document.createElement('div')
 if (!MainContentContainer.dataset['legacy']) {
-    MainContent.classList.add('page--container')
-    MainContentContainer.append(MainContent)
+  MainContent.classList.add('page--container')
+  MainContentContainer.append(MainContent)
 
-    const page = require('@a2nt/mithril-ui/src/js/ui/page/tpl')
-    m.mount(MainContent, page)
+  const page = require('@a2nt/mithril-ui/src/js/ui/page/tpl')
+  m.mount(MainContent, page)
 } else {
-    // legacy fallback
-    const spinner = document.getElementById('PageLoading')
-    if (spinner) {
-        window.addEventListener(`${Events.LOADED}`, () => {
-            spinner.classList.add('d-none')
-        })
+  // legacy fallback
+  const spinner = document.getElementById('PageLoading')
+  if (spinner) {
+    window.addEventListener(`${Events.LOADED}`, () => {
+      spinner.classList.add('d-none')
+    })
 
-        window.addEventListener(`${Events.AJAX}`, () => {
-            spinner.classList.add('d-none')
-        })
-    }
+    window.addEventListener(`${Events.AJAX}`, () => {
+      spinner.classList.add('d-none')
+    })
+  }
 }
