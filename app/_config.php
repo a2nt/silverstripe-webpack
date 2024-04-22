630672e87ece0bd18193d4472a83046eaebca064
Tony Air <tony@twma.pro>
2024-04-22 15:02:31 +0200

    UPD: Minor edits

--- app/_config.php
+++ app/_config.php
@@ -1,5 +1,10 @@
 <?php
 
+use SilverStripe\Core\EnvironmentLoader;
+
+$env = BASE_PATH . '/app/.env';
+$loader = new EnvironmentLoader();
+$loader->loadFile($env);
 /*\SilverStripe\Core\Config\Config::modify()->set(
     \SilverStripe\Control\Email\Email::class,
     'bcc_all_emails_to',//'cc_all_emails_to',
