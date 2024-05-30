dab46354883ae6f5d5d6a51214e8291d1ef59666
Tony Air <tony@twma.pro>
2024-05-30 18:16:13 +0200

    UPD: TinyMCE valid_elements

--- app/_config.php
+++ app/_config.php
@@ -1,5 +1,6 @@
 <?php
 
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 use SilverStripe\Core\EnvironmentLoader;
 
 $env = BASE_PATH . '/app/.env';
@@ -10,3 +11,20 @@ $loader->loadFile($env);
     'bcc_all_emails_to',//'cc_all_emails_to',
     'test@example.com'
 );*/
+
+
+$cfg = TinyMCEConfig::get('cms');
+$cfg->setOption('forced_root_block', 'p');
+$cfg->setOption(
+    'valid_elements',
+    'strong/b,em/i,a[href|target=_blank],p/div,br,' .
+    'blockquote,' .
+    'img[class|src|alt|title|hspace|vspace|width|height|align|name|usemap|data*],' .
+    'iframe[src|name|width|height|align|frameborder|marginwidth|marginheight|scrolling],' .
+    'object[width|height|data|type],' .
+    'embed[src|type|pluginspage|width|height|autoplay],' .
+    'param[name|value],' .
+    'map[class|name|id],' .
+    'area[shape|coords|href|target|alt],' .
+    'ol[start|type]'
+);
